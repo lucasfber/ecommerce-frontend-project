d806723e11e5a71ee49dfb71cd48771242805822
Lucas Bertoldo <lucas.fber@gmail.com>
2019-08-14 13:52:43 -0300

    modal login done, dropdown menu for currency and language coding

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,40 @@
-function setCurrentYear() {
-  let currentYear = $('#current-year');
-  currentYear.text(new Date().getFullYear());
-}
-setCurrentYear();
+$(document).ready(function() {
+  let togglerPassword = $('#toggler');
+  let inputPassword = $('#password');
+  let userIcon = $('.flaticon-man');
+  let modalLogin = $('.modal-login');
+  let modalLoginForm = $('.modal-login__form'); // is this variable really necessary?
+
+  function setCurrentYear() {
+    let currentYear = $('#current-year');
+    currentYear.text(new Date().getFullYear());
+  }
+
+  function showPassword() {
+    inputPassword.attr('type', 'text');
+  }
+
+  function hidePassword() {
+    inputPassword.attr('type', 'password');
+  }
+
+  modalLogin.click(function(e) {
+    if (e.target === this) {
+      modalLogin.css('display', 'none');
+      modalLoginForm.trigger('reset');
+    }
+  });
+
+  userIcon.click(function() {
+    modalLogin.css('display', 'flex');
+  });
+
+  togglerPassword.mousedown(function() {
+    showPassword();
+  });
+  togglerPassword.mouseup(function() {
+    hidePassword();
+  });
+
+  setCurrentYear();
+});
