2ceb0dfab29c5992dcfb28852301804c55cda4eb
Lucas Bertoldo <lucas.fber@gmail.com>
2019-08-15 21:16:24 -0300

    dropdown menu done

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,32 +1,51 @@
 $(document).ready(function() {
-  let togglerPassword = $('#toggler');
-  let inputPassword = $('#password');
-  let userIcon = $('.flaticon-man');
-  let modalLogin = $('.modal-login');
-  let modalLoginForm = $('.modal-login__form'); // is this variable really necessary?
+  const dropdown = $(".menu__first-level-item");
+  let togglerPassword = $("#toggler");
+  let inputPassword = $("#password");
+  let userIcon = $(".flaticon-man");
+  let modalLogin = $(".modal-login");
+  let modalLoginForm = $(".modal-login__form"); // is this variable really necessary?
 
   function setCurrentYear() {
-    let currentYear = $('#current-year');
+    let currentYear = $("#current-year");
     currentYear.text(new Date().getFullYear());
   }
 
+  function toggleDropdown(e) {
+    $(".dropdown").hide();
+    e.stopPropagation();
+    $(e.currentTarget)
+      .children(".dropdown")
+      .toggle();
+  }
+
   function showPassword() {
-    inputPassword.attr('type', 'text');
+    inputPassword.attr("type", "text");
   }
 
   function hidePassword() {
-    inputPassword.attr('type', 'password');
+    inputPassword.attr("type", "password");
+  }
+
+  function showModal() {
+    modalLogin.css("display", "flex");
+  }
+
+  function handleClickOutsideModal() {
+    $(window).click(function() {
+      $(".dropdown").hide();
+    });
   }
 
   modalLogin.click(function(e) {
     if (e.target === this) {
-      modalLogin.css('display', 'none');
-      modalLoginForm.trigger('reset');
+      modalLogin.css("display", "none");
+      modalLoginForm.trigger("reset");
     }
   });
 
   userIcon.click(function() {
-    modalLogin.css('display', 'flex');
+    showModal();
   });
 
   togglerPassword.mousedown(function() {
@@ -36,5 +55,10 @@ $(document).ready(function() {
     hidePassword();
   });
 
+  dropdown.click(function(e) {
+    toggleDropdown(e);
+  });
+
   setCurrentYear();
+  handleClickOutsideModal();
 });
